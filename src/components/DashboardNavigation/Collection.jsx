c164cb48fb3b37e4d57e47040c990e0a61d76d2d
Felix <dexdaprotocol@gmail.com>
2022-09-06 12:56:48 +0200

    collection fetching

--- src/components/DashboardNavigation/Collection.jsx
+++ src/components/DashboardNavigation/Collection.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { useMoralis } from "react-moralis";
+
 const products = [
   {
     id: 1,
@@ -113,7 +116,36 @@ const products = [
 ];
 
 export default function Example() {
+  const { Moralis } = useMoralis();
+  const [selectedPlayer, setSelectedPlayer] = useState();
+  const [player, setPlayer] = useState([]);
+
+  useEffect(() => {
+    const Player = Moralis.Object.extend("Player");
+    const query = new Moralis.Query(Player);
+    query.find().then((results) => {
+      let r = [];
+      // let rmap = new Map();
+      results.forEach((result) => {
+        r.push({
+          id: result.id,
+          Name: result.get("name"),
+          Position: result.get("position"),
+          NftId: result.get("nftId"),
+          Number: result.get("number"),
+          Image: result.get("image"),
+          Team: result.get("team"),
+          Type: result.get("type"),
+        });
+        // rmap[result.get("Player")] = result.id;
+      });
+      setPlayer(r);
+      // setSelectedPlayerId(rmap);
+      console.log(player);
+    });
+  }, []);
   function sellPlayer() {
+    alert("selected");
     // pop up modal for selling a player on the marketplace
   }
   return (
@@ -124,30 +156,46 @@ export default function Example() {
         </h2>
 
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <div key={product.id} className="group relative">
-              <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+          {player.map((card, cardIdx) => (
+            <div
+              key={cardIdx}
+              className="group relative ring-gray-500 ring-1 rounded-lg pb-2"
+            >
+              <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-t-md overflow-hidden lg:h-80 lg:aspect-none">
                 <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
+                  src={"/CR7.png"}
                   className="w-full h-full object-center object-cover lg:w-full lg:h-full"
                 />
               </div>
-              <div className="mt-4 flex justify-between">
+              <div className="mt-4 pb-1 border-b-2 flex justify-between mx-2">
                 <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
+                  <div className="text-md text-gray-700">
+                    <div>
                       <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                      {card.Name}
+                    </div>
+                  </div>
+                  {/* <p className="mt-1 text-sm text-gray-500">{product.color}</p> */}
+                </div>
+                <div className="text-md font-medium cursor-pointer text-gray-900">
+                  {card.Number}
+                </div>
+              </div>
+              <div className="flex items-center justify-around  mt-2">
+                <div
+                  className={`inline-flex items-center px-2 py-1 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 ${
+                    card.Type == "legendary"
+                      ? "text-yellow-500"
+                      : "text-gray-500"
+                  }`}
+                >
+                  {card.Type}
                 </div>
                 <button
                   onClick={sellPlayer}
-                  className="text-sm font-medium text-gray-900"
+                  className="inline-flex z-50 items-center px-2 py-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
                 >
-                  {product.price}
+                  Sell
                 </button>
               </div>
             </div>
