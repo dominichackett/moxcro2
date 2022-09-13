ef511be6eaf675da66337703df93a1a7104ad837
dominichackett <dominichackett@gmail.com>
2022-09-12 20:29:09 -0400

    Update TeamList.jsx
    Teams can be viewed by selecting them from the drop down list.

--- src/components/DashboardNavigation/TeamList.jsx
+++ src/components/DashboardNavigation/TeamList.jsx
@@ -2,23 +2,17 @@ import { Fragment, useEffect, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { useMoralis } from "react-moralis";
 
-const people = [
-  { name: "Wade Cooper" },
-  { name: "Arlene Mccoy" },
-  { name: "Devon Webb" },
-  { name: "Tom Cook" },
-  { name: "Tanya Fox" },
-  { name: "Hellen Schmidt" },
-];
 
 export default function TeamList() {
   const { Moralis } = useMoralis();
-  const [selected, setSelected] = useState(people[0]);
+  const [selected, setSelected] = useState();
+  const [gotTeams,setGotTeams] = useState(false);
   const [teams, setTeams] = useState([]);
-
+  const [players,setPlayers] = useState([])
   useEffect(() => {
     const Team = Moralis.Object.extend("Team");
     const query = new Moralis.Query(Team);
+    query.ascending("name");
     query.find().then((results) => {
       let r = [];
       results.forEach((result) => {
@@ -28,58 +22,122 @@ export default function TeamList() {
         });
       });
       setTeams(r);
+      setGotTeams(true);
+      setSelected(r[0].id)
     });
     console.log(teams);
   }, []);
 
+
+  useEffect(()=>{
+    if(gotTeams)
+    {
+       const Player = Moralis.Object.extend("Player");
+       const Team = Moralis.Object.extend("Team");
+       const team = new Team();
+       team.id = selected;
+       const query = new Moralis.Query(Player);
+       query.equalTo("team",team);
+       query.include("team");
+       query.ascending("position");
+       query.find().then((results)=>{
+         let p =[]; 
+         results.forEach(result =>{
+             p.push(result);
+          })
+          setPlayers(p);
+          setGotTeams(true)
+       })           
+    }
+
+  },[selected])
+  function change(){
+    alert(selected)
+  }
+  const handleChange = event => {
+    setSelected(event.target.value);
+  };
+  function playerPosition(pos)
+{
+	 switch(pos){
+		 
+		 case 0: return("Goalkeeper");
+		 break;
+		 
+		 case 1: return("Defender");
+		 break;
+		  case 2: return("Midfielder");
+		 break;
+		 
+		 case 3: return("Forward");
+		 break;
+	 }
+}
   return (
-    <div className=" w-72">
-      <Listbox value={selected} onChange={setSelected}>
-        <div className="relative mt-1">
-          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-            <span className="block truncate">
-              {selected ? selected : "Select Team"}
-            </span>
-          </Listbox.Button>
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-              {teams.map((team, index) => (
-                <Listbox.Option
-                  key={index}
-                  className={({ active }) =>
-                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                      active ? "bg-amber-100 text-amber-900" : "text-gray-900"
-                    }`
-                  }
-                  value={team.name}
+    <div >
+      {selected?.Name}
+       <select
+                  id="country"
+                  value={selected} onChange={handleChange}
+                  name="country"
+                  autoComplete="country-name"
+                  className="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm"
                 >
-                  {({ selected }) => (
-                    <>
-                      <span
-                        className={`block truncate ${
-                          selected ? "font-medium" : "font-normal"
-                        }`}
-                      >
-                        {team.name}
-                      </span>
-                      {selected ? (
-                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                          {/* <CheckIcon className="h-5 w-5" aria-hidden="true" /> */}
-                        </span>
-                      ) : null}
-                    </>
-                  )}
-                </Listbox.Option>
-              ))}
-            </Listbox.Options>
-          </Transition>
-        </div>
-      </Listbox>
+                   {teams.map((team, index) => (
+                  <option value={team.id}>{team.Name}</option>))}
+                  
+                </select>
+                <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+
+                <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {players.map((card, cardIdx) => (
+            <div
+              key={cardIdx}
+              className="group relative ring-gray-500 ring-1 rounded-lg pb-2"
+            >
+              <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-t-md overflow-hidden lg:h-80 lg:aspect-none">
+                <img
+                  src={card.get("image")}
+                  className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                />
+              </div>
+              <div className="mt-4 pb-1 border-b-2 flex justify-between mx-2">
+                <div>
+                  <div className="text-md text-gray-700">
+                    <div>
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {card.get("name")}
+                    </div>
+                    <div className="flex space-x-8 flex-row w-full items-center justify-center">
+                      <div className="font-bold">
+                        {card.get("team").get("name")}
+                      </div>
+                      <div className="text-xs">
+                        [{playerPosition(card.get("position"))}]
+                      </div>
+                    </div>
+                  </div>
+                  {/* <p className="mt-1 text-sm text-gray-500">{product.color}</p> */}
+                </div>
+                <div className="text-md font-medium cursor-pointer text-gray-900">
+                  {card.get("number")}
+                </div>
+              </div>
+              <div className="flex items-center justify-around  mt-2">
+                <div
+                  className={`inline-flex items-center px-2 py-1 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 ${
+                    card.get("type") == "legendary"
+                      ? "text-yellow-500"
+                      : "text-gray-500"
+                  }`}
+                >
+                  {card.get("type").toUpperCase()}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>       
     </div>
+</div>
   );
 }
